ec663d10a23aaca36b60f6b15765565437714cfe
Satyam <Naithani>
2021-03-27 03:01:06 +0530

    dashboard-boilerplate

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import cakeReducer from './component1/comp1Reducer';
 import iceCreamReducer from './component2/comp2Reducer';
-import userReducer from './component3/comp3Reducer'
+import userReducer from './component3/comp3Reducer';
 
 const rootReducer = combineReducers({
   cake: cakeReducer,
